0e81086b570762ee45e327597d4b9e344b439375
Felipe <felipe@visionar.io>
2025-01-21 17:12:41 -0300

    first commit

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meus Lugares'),
+        actions: [],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
