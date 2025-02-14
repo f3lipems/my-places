b6059eb48b885c459b6c97ed7374b12f9fd9b4d1
Felipe <felipe@visionar.io>
2025-02-14 18:29:01 -0300

    add image picker

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,6 +11,23 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+
+  _takePicture() async {
+    final ImagePicker _picker = ImagePicker();
+
+    final XFile? _imageFile = await _picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+
+    if (_imageFile == null) return;
+
+    setState(() {
+      _storedImage = File(_imageFile.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -23,7 +43,13 @@ class _ImageInputState extends State<ImageInput> {
             ),
           ),
           alignment: Alignment.center,
-          child: const Text('Nenhuma imagem!'),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : const Text('Nenhuma imagem!'),
         ),
         Expanded(
           child: ElevatedButton.icon(
@@ -34,7 +60,7 @@ class _ImageInputState extends State<ImageInput> {
                 color: Theme.of(context).primaryColor,
               ),
             ),
-            onPressed: () {},
+            onPressed: _takePicture,
           ),
         ),
       ],
