311f283e84d4791bc8cf2a68989a45d3e00168f1
Felipe <felipe@visionar.io>
2025-02-27 19:23:18 -0300

    add select position on map

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -3,18 +3,36 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:my_places/models/place.dart';
 
 class MapScreen extends StatefulWidget {
+
+  final PlaceLocation? initialLocation;
+  final bool isReadOnly;
+  
   const MapScreen({
     super.key,
     this.initialLocation = const PlaceLocation(latitude: 37.419857, longitude: -122.078827, address: ''),
+    this.isReadOnly = false,
   });
-
-  final PlaceLocation? initialLocation;
 
   @override
   State<MapScreen> createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
+
+  LatLng? _pickedPosition;
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedPosition = position;
+    });
+
+    // Navigator.of(context).pop(PlaceLocation(
+    //   latitude: position.latitude,
+    //   longitude: position.longitude,
+    //   address: '',
+    // ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +44,13 @@ class _MapScreenState extends State<MapScreen> {
           target: LatLng(widget.initialLocation!.latitude, widget.initialLocation!.longitude),
           zoom: 13,
         ),
+        onTap: widget.isReadOnly ? null : _selectLocation,
+        markers: {
+          Marker(
+            markerId: const MarkerId('p1'),
+            position: _pickedPosition ?? LatLng(widget.initialLocation!.latitude, widget.initialLocation!.longitude),
+          ),
+        },
       ),
     );
   }
